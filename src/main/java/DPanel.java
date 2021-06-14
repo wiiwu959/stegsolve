de3c8abb8766e94b285a76a1d820a95cfcd732d8
wiiwu959 <iriswu959@gmail.com>
2021-06-15 02:26:35 +0800

    add menu test.

--- src/main/java/DPanel.java
+++ src/main/java/DPanel.java
@@ -59,7 +59,7 @@ public class DPanel extends JPanel
         defaultSize.width = bi.getWidth();
         defaultSize.height = bi.getHeight();
         setSize(bi.getWidth(), bi.getHeight());
-        apply(100); // BugFix.
+        apply(100);
         repaint();
     }
 
--- src/test/java/StegSolveTest.java
+++ src/test/java/StegSolveTest.java
@@ -1,9 +1,19 @@
+import org.assertj.swing.core.GenericTypeMatcher;
+import org.assertj.swing.core.KeyPressInfo;
+import org.assertj.swing.driver.JMenuItemMatcher;
 import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.fixture.JFileChooserFixture;
+import org.assertj.swing.fixture.JMenuItemFixture;
+import org.assertj.swing.fixture.JPopupMenuFixture;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.*;
 
 public class StegSolveTest {
@@ -16,11 +26,99 @@ public class StegSolveTest {
         Assert.assertTrue(Modifier.isPrivate(stegSolveConstructor.getModifiers()));
         stegSolveConstructor.setAccessible(true);
         frame = new FrameFixture(stegSolveConstructor.newInstance());
+        frame.show();
     }
 
     @Test
-    public void test(){
+    public void testOpen(){
+        JMenuItemFixture openItem = frame.menuItem(new GenericTypeMatcher<JMenuItem>(JMenuItem.class) {
+            @Override
+            protected boolean isMatching(JMenuItem item) {
+                return "Open".equals(item.getText());
+            }
+        });
+        openItem.click();
 
+        JFileChooserFixture fileChooserFixture = frame.fileChooser();
+        fileChooserFixture.setCurrentDirectory(new File("src/test/testcase/"));
+        fileChooserFixture.fileNameTextBox().setText("minion.jpg");
+        fileChooserFixture.pressAndReleaseKey(KeyPressInfo.keyCode(KeyEvent.VK_ENTER));
+    }
+
+    @Test
+    public void testFileFormat(){
+        ((StegSolve) frame.target()).loadImage("src/test/testcase/minion.jpg");
+
+        JMenuItemFixture item = frame.menuItem(new GenericTypeMatcher<JMenuItem>(JMenuItem.class) {
+            @Override
+            protected boolean isMatching(JMenuItem item) {
+                return "File Format".equals(item.getText());
+            }
+        });
+        item.click();
+    }
+
+    @Test
+    public void testDataExtract(){
+        ((StegSolve) frame.target()).loadImage("src/test/testcase/minion.jpg");
+
+        JMenuItemFixture item = frame.menuItem(new GenericTypeMatcher<JMenuItem>(JMenuItem.class) {
+            @Override
+            protected boolean isMatching(JMenuItem item) {
+                return "Data Extract".equals(item.getText());
+            }
+        });
+        item.click();
+    }
+
+    @Test
+    public void testStereogramSolver(){
+        ((StegSolve) frame.target()).loadImage("src/test/testcase/minion.jpg");
+
+        JMenuItemFixture item = frame.menuItem(new GenericTypeMatcher<JMenuItem>(JMenuItem.class) {
+            @Override
+            protected boolean isMatching(JMenuItem item) {
+                return "Stereogram Solver".equals(item.getText());
+            }
+        });
+        item.click();
+    }
+
+    @Test
+    public void testFrameBrowser(){
+        ((StegSolve) frame.target()).loadImage("src/test/testcase/minion.jpg");
+
+        JMenuItemFixture item = frame.menuItem(new GenericTypeMatcher<JMenuItem>(JMenuItem.class) {
+            @Override
+            protected boolean isMatching(JMenuItem item) {
+                return "Frame Browser".equals(item.getText());
+            }
+        });
+        item.click();
+    }
+
+    @Test
+    public void testImageCombiner(){
+        ((StegSolve) frame.target()).loadImage("src/test/testcase/minion.jpg");
+
+        JMenuItemFixture item = frame.menuItem(new GenericTypeMatcher<JMenuItem>(JMenuItem.class) {
+            @Override
+            protected boolean isMatching(JMenuItem item) {
+                return "Image Combiner".equals(item.getText());
+            }
+        });
+        item.click();
+    }
+
+    @Test
+    public void testAbout() throws IOException {
+        JMenuItemFixture item = frame.menuItem(new GenericTypeMatcher<JMenuItem>(JMenuItem.class) {
+            @Override
+            protected boolean isMatching(JMenuItem item) {
+                return "About".equals(item.getText());
+            }
+        });
+        item.click();
     }
 
     @After
