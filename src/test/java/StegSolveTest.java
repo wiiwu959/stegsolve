88179db551cf5a890e73ba01e9b9d8a72032d763
weicheng <jordan860412@gmail.com>
2021-06-15 02:19:44 +0800

    Add button and key press test in StegSolveTest.

--- src/test/java/StegSolveTest.java
+++ src/test/java/StegSolveTest.java
@@ -1,9 +1,15 @@
-import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.core.KeyPressInfo;
+import org.assertj.swing.core.matcher.JButtonMatcher;
+import org.assertj.swing.fixture.*;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 import java.lang.reflect.*;
 
 public class StegSolveTest {
@@ -16,11 +22,52 @@ public class StegSolveTest {
         Assert.assertTrue(Modifier.isPrivate(stegSolveConstructor.getModifiers()));
         stegSolveConstructor.setAccessible(true);
         frame = new FrameFixture(stegSolveConstructor.newInstance());
+        frame.show();
     }
 
     @Test
-    public void test(){
+    public void testForward() {
+        JButtonFixture btn = frame.button(JButtonMatcher.withText(">"));
+        btn.requireVisible();
+        btn.requireEnabled();
+        btn.click();
 
+        ((StegSolve) frame.target()).loadImage("src/test/testcase/minion.jpg");
+        btn.click();
+    }
+
+    @Test
+    public void testBack() {
+        JButtonFixture btn = frame.button(JButtonMatcher.withText("<"));
+        btn.requireVisible();
+        btn.requireEnabled();
+        btn.click();
+
+        ((StegSolve) frame.target()).loadImage("src/test/testcase/minion.jpg");
+        btn.click();
+    }
+
+    @Test
+    public void testKeyLeft() {
+        frame.pressAndReleaseKey(KeyPressInfo.keyCode(KeyEvent.VK_LEFT));
+
+        ((StegSolve) frame.target()).loadImage("src/test/testcase/minion.jpg");
+        frame.pressAndReleaseKey(KeyPressInfo.keyCode(KeyEvent.VK_LEFT));
+    }
+
+    @Test
+    public void testKeyRight() {
+        frame.pressAndReleaseKey(KeyPressInfo.keyCode(KeyEvent.VK_RIGHT));
+
+        ((StegSolve) frame.target()).loadImage("src/test/testcase/minion.jpg");
+        frame.pressAndReleaseKey(KeyPressInfo.keyCode(KeyEvent.VK_RIGHT));
+    }
+
+    @Test
+    public void testKeyEnter(){
+        JTextComponentFixture textField = frame.textBox();
+        textField.setText("200");
+        textField.pressAndReleaseKey(KeyPressInfo.keyCode(KeyEvent.VK_ENTER));
     }
 
     @After
